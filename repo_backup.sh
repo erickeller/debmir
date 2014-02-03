029e5e1f6c71ecfca3356ea9767af8bdbca3ba34
Eric Keller <eric.keller.ek1@roche.com>
2014-02-03 13:33:04 +0100

    add new repo_backup script backuping complete repository with rsync

---
+++ repo_backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# this script does use rsync to make a backup
+
+usage()
+{
+  echo "
+usage: $0 from to
+
+example:
+$0 /repo /repo_backup
+"
+exit 1
+}
+
+
+if [ "$#" -ne 2 ]
+then
+  usage
+fi
+
+RSYNC_OPTION="--verbose --checksum --recursive --links --hard-links --perms --executability --group --times --compress --progress"
+FROM=$1
+TO=$2
+echo "rsync from: ${FROM} to: ${TO}
+Using options: ${RSYNC_OPTION}"
+
+rsync ${RSYNC_OPTION} ${FROM} ${TO}
+
